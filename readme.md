a2ed29ef211db9f309ef806ebe3602b20fa5ed4e
chibuzor christian <gigzblog@gmail.com>
2019-10-02 18:56:09 +0100

    Update

--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
-# youtube_graph
 
 ## ABOUT
 
--- src/components/core/lib/client/jsonClient.js
+++ src/components/core/lib/client/jsonClient.js
@@ -1,78 +1,12 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-console */
 
-/**
-* Every client should return an object with atleast four properties
-* 1. A search method which takes  3 mandatory parameters (query, success_callback, error_callback)
-* 2. A searchlink method  which takes  4  mandatory parameters (id ,  maxResult, success_callback, error_callback)
-* 3. A getItemId method that returns the unique identifer of every item 
-* 4. A extractData method 
-*/
 import httpGet from './client';
 
-/**
-* 
-* @param {Object} video 
-*/
-function getItemId(item) {
-    return item.id;
-}
-/**
-* 
-* @param {*} data 
-*/
-function extractData(data) {
-    const image = data.thumbnail
-    const width = 60;
-    const ratio = width / image.width;
-    return {
-        image,
-        width,
-        height: image.height * ratio,
-        isPinned: data.isPinned,
-        title: data.title,
-        info: data.info
-    };
-}
-export default function (request) {
-    let jsonFile = request.fileName
-    const origin = request.origin;
-    function buildParameters(request) {
-        request.fname = jsonFile
-        return Object.keys(request).map(key => {
-            return encodeURIComponent(key) + '=' + encodeURIComponent(request[key]);
-        }).join('&');
-    }
-    function makeRequestToOrigin(origin, request, callback, error) {
-        const parameters = buildParameters(request);
-        if (parameters.length === 0) {
-            throw new Error('Invalid request arguments');
-        }
-        const dataUrl = origin + (origin.indexOf('?') === -1 ? '?' : '&') + parameters;
-        return httpGet(dataUrl).then(msg => {
-            callback(JSON.parse(msg));
-        }).catch((err) => {
-            error(err);
-        });
-    }
-    function makeRequest(request, callback, error) {
-        return makeRequestToOrigin(origin, request, callback, error);
-    }
-    /**
-    * 
-    * @param {String} query 
-    * @param {Function} callback 
-    * @param {Function} error 
-    * @param {Objec} extra 
-    */
-    function searchJson(query, callback, error, extra) {
-        const args = Object.assign({
-            query: query,
-            related: 0,
-            maxResults: 10
-        }, extra);
 
-        return makeRequest(args, callback, error);
+export default class {
+    constructor(request) {
+        this.client = httpGet
+        this.jsonFile = request.fileName
+        this.origin = request.origin;
     }
     /**
     * 
@@ -80,18 +14,79 @@ export default function (request) {
     * @param {Number} maxResults 
     * @param {Function} callback 
     * @param {Function} error 
+    * @param {Object} extra 
     */
-    function findRelatedItems(itemId, maxResults, callback, error) {
-        return makeRequest({
+    link(itemId, maxResults, callback, error, extra) {
+        const args = Object.assign({
             maxResults: (typeof maxResults === 'number' ? maxResults : 10),
             related: 1,
-            relatedToItemId: itemId
-        }, callback, error);
+            relatedToItemId: itemId,
+            backQuery:this.query
+        }, extra)
+        return this.makeRequest(args, callback, error);
     }
-    return {
-        search: searchJson,
-        link: findRelatedItems,
-        getItemId: getItemId,
-        extractData: extractData
+    /**
+    * 
+    * @param {String} query 
+    * @param {Function} callback 
+    * @param {Function} error 
+    * @param {Object} extra 
+    */
+    search(query, callback, error, extra) {
+        this.query = query
+        const args = Object.assign({
+            query: this.query,
+            related: 0,
+            maxResults: 10
+        }, extra);
+
+        return this.makeRequest(args, callback, error);
     }
+    /**
+    * 
+    * @param {Object} data 
+    */
+    extractData(data) {
+        const image = data.thumbnail
+        const width = 60;
+        const ratio = width / image.width;
+        return {
+            image,
+            width,
+            height: image.height * ratio,
+            isPinned: data.isPinned,
+            title: data.title,
+            info: data.info
+        }
+    }
+    /**
+    * 
+    * @param {Object} item 
+    */
+    getItemId(item) {
+        return item.id;
+    }
+
+    makeRequest(request, callback, error) {
+        return this.makeRequestToOrigin(request, callback, error);
+    }
+    makeRequestToOrigin(request, callback, error) {
+        const parameters = this.buildParameters(request);
+        if (parameters.length === 0) {
+            throw new Error('Invalid request arguments');
+        }
+        const dataUrl = this.origin + (this.origin.indexOf('?') === -1 ? '?' : '&') + parameters;
+        return this.client(dataUrl).then(msg => {
+            callback(JSON.parse(msg));
+        }).catch((err) => {
+            error(err);
+        });
+    }
+    buildParameters(request) {
+        request.fname = this.jsonFile
+        return Object.keys(request).map(key => {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(request[key]);
+        }).join('&');
+    }
+
 }
